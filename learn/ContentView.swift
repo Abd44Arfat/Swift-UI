bd69ea56df448e549a5ac77a011a440c7dbc2b7d
Abd44Arfat <abdoarfat924@gmail.com>
2025-03-23 10:59:31 +0200

    first commit on swift ui

--- learn/ContentView.swift
+++ learn/ContentView.swift
@@ -11,17 +11,12 @@ struct ContentView: View {
     var body: some View {
         VStack (alignment: .leading){
            
-            Text("welcome,!")
-                .font(.title)
-                .foregroundColor(Color.blue)
+           
             HStack {
                 Text("Hello,abderahman !")
                     .font(.subheadline)
                     .foregroundColor(Color.blue)
-                Spacer()
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
+          
             }
           
         }
